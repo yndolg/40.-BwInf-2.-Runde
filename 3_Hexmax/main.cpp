5cac0ba81e757ea25d3c7e549b0c1fb946ccece9
yndolg <33730531+yndolg@users.noreply.github.com>
2022-03-08 17:19:02 +0100

    Eingabe und main.cpp für 3.

---
+++ 3_Hexmax/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <fstream>
+#include <random>
+#include "Solver.h"
+#include "DPSolver.h"
+
+using namespace std;
+int main() {
+    std::ifstream ifs("../Eingabe/hexmax4.txt");
+    std::string s;
+    ifs >> s;
+    int c;
+    ifs >> c;
+    DPSolver solver;
+
+    auto r = solver.solve(s, c);
+
+    cout << "Solution: " << r << "\n";
+    cout << "Moves: \n";
+    cout << solver.getMoves(s, r);
+    return 0;
+}
